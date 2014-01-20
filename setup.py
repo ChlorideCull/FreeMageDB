1dc43d6f11ae4aed04902ebf22680b36c2b4b87e
ChlorideCull <chloride@devurandom.net>
2014-01-20 23:21:41 +0100

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+]
+
+setup(name="FreeMageDB",
+        version="1.0",
+        description="FreeMage - A Free File Database Back-End",
+        author="Chloride Cull",
+        author_email="chloride@devurandom.net",
+        url="https://github.com/ChlorideCull/FreeMageDB",
+        classifiers=classifiers,
+        packages=['FreeMage']
+        )
