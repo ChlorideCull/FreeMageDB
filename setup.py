7d2d9b654fe97278b31605c738f656123e73d07b
ChlorideCull <chloride@devurandom.net>
2014-01-21 21:26:10 +0100

    bump version number - semantic versioning

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 ]
 
 setup(name="FreeMageDB",
-        version="1.0",
+        version="1.1",
         description="FreeMage - A Free File Database Back-End",
         author="Chloride Cull",
         author_email="chloride@devurandom.net",
